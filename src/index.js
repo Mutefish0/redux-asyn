139ff7359f91baf7304204ace1b2547e91712c6d
riddleoo <648262030@qq.com>
2016-05-21 01:13:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 * redux-asyn
 * */
 
-export default () => next => action =>{
-    next(action)
-    let { type, asyn, ...param } = action
-    if(asyn) asyn({ dispatch : next, ...param })
-    return action
+export default function asynMiddleware(){ 
+    return next => action =>{
+        next(action)
+        let { type, asyn, ...param } = action
+        if(asyn) asyn({ dispatch : next, ...param })
+        return action
+    }
 }
