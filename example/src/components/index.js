a2abaf99d2eff01dd7c83f11305ebe313264e90c
riddleoo <648262030@qq.com>
2016-05-21 01:29:06 +0800

    update

--- example/src/components/index.js
+++ example/src/components/index.js
@@ -10,8 +10,8 @@ const App = ({ status, errorInfo, login }) => {
         <div style={{textAlign:'center'}}>
             <span> status : {status} </span> <br/>
             <span style={{display:errorInfo?'inline':'none'}}>message : {errorInfo} </span> <br/>
-            <input type="text" placeholder="username" ref={node => userName = node}/> <br/>
-            <input type="password" placeholder="password" ref={node => pwd = node}/> <br/>
+            <input type="text" placeholder="username is hello" ref={node => userName = node}/> <br/>
+            <input type="password" placeholder="password is 123456" ref={node => pwd = node}/> <br/>
             <button onClick={()=>login(userName.value,pwd.value)}> Login </button>
         </div>
     )
