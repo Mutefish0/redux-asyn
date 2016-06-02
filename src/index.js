9890dcb318361235ae1f3ca545be6bfa8e09b90d
riddleoo <648262030@qq.com>
2016-06-02 22:30:13 +0800

    add `Promise` support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 /*
-* redux-asyn
+* redux-asyn updated
 * */
 
 export default function asynMiddleware(){ 
     return next => action =>{
-        next(action)
+        next(action) 
         let { type, asyn, ...param } = action
-        if(asyn) asyn({ dispatch : next, ...param })
+        if(asyn) 
+            {
+              let promise = asyn({ dispatch : next, ...param })
+              if(promise instanceof Promise) promise.then((nextAction)=>next(nextAction))
+            }
         return action
     }
 }
